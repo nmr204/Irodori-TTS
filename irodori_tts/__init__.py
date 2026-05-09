1ae40bc4296e8bdfcd18ff4810aed98a127105b6
nmr204 <ynomura.ikegami@gmail.com>
2026-05-10 01:34:27 +0900

    Update __init__.py

--- irodori_tts/__init__.py
+++ irodori_tts/__init__.py
@@ -1,12 +1,10 @@
 """Irodori-TTS package: text-conditioned RF diffusion over DACVAE latents."""
 
 from .config import ModelConfig, SamplingConfig, TrainConfig
-from .lora import LORA_TARGET_PRESETS
 from .model import TextToLatentRFDiT
 from .tokenizer import ByteTokenizer, PretrainedTextTokenizer
 
 __all__ = [
-    "LORA_TARGET_PRESETS",
     "ByteTokenizer",
     "ModelConfig",
     "PretrainedTextTokenizer",
